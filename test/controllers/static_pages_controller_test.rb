ab570992519ecf163e9f1eb6b275f7a8061a3c12
MaxMeira <maxdawis@gmail.com>
2015-05-07 21:40:27 -0300

    Finish layout and routs

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select  "title" ,  "Home | AOS App"
+    assert_select  "title" ,  "Home | Academic Orientation System"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select  "title" ,  "Help | AOS App"
+    assert_select  "title" ,  "Help | Academic Orientation System"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select  "title" ,  "About | AOS App"
+    assert_select  "title" ,  "About | Academic Orientation System"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Academic Orientation System"
   end
 end
