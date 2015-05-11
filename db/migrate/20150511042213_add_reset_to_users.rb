70403ee4d850d1bc250fe7e4379c3d50d2409715
MaxMeira <maxdawis@gmail.com>
2015-05-11 02:42:25 -0300

    Add password resets & email configuration

---
+++ db/migrate/20150511042213_add_reset_to_users.rb
@@ -0,0 +1,9 @@
+class AddResetToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :reset_digest, :string
+    add_column :users, :reset_sent_at, :datetime
+    #add_column :users, :reset_digest, :string
+    #add_column :users, : , :string
+    #add_column :users, :reset_sent_at, :datetime
+  end
+end
